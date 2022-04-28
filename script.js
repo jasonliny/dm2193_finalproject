a66650f46266b8a17789fd4b4e6da02f36eabb5a
unknown <70929180+jasonliny@users.noreply.github.com>
2022-04-28 08:11:04 -0400

    Remove commented-out code and log statments used for testing

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 document.addEventListener('aos:in', ( {detail} ) => {
-  console.log('animated in');//, detail);
   detail.style.animation="";
-  // console.log(detail.querySelectorAll(".boxes div"));
   for (ele of detail.querySelectorAll(".boxes div")) {
     ele.classList.toggle("animate");
     ele.style.animation="";
-    console.log(ele);
   }
 });
 document.addEventListener('aos:out', ( {detail} ) => {
-  console.log('animated out');//, detail);
-  // detail.style.animation="none";
   for (ele of detail.querySelectorAll(".boxes div")) {
-    console.log(ele);
-    // ele.style.fillMode="none";
-    // console.log(ele.style.fillMode);
     ele.classList.toggle("animate");
   }
 });
